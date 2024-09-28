3683f1a36b6baca17ddc6552928c627a35da3229
Connor Bell (Makeshift) <git@connor-bell.com>
2024-09-28 20:43:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# AWS SSO Device Authorization Grant Provider
+
+This package provides a simple way to get credentials for AWS services using the AWS SSO device authorization grant flow. This is useful for CLI tools that need to access AWS services on behalf of a user.
+
+If you've ever used `aws sso login` to get credentials for the AWS CLI, this package does the same thing, but doesn't require the AWS CLI to be installed and can be configured programmatically.
+
+Thanks to [this fantastic blog post](https://medium.com/@lex.berger/anatomy-of-aws-sso-device-authorization-grant-2839008c367a) by Alex Berger for the explanation of how AWS SSO device authorization grants work.
+
+## Installation
+
+Yarn: `yarn add aws-sso-device-auth-provider`
+Npm: `npm install aws-sso-device-auth-provider`
+
+## Requirements
+
+The user intending to execute the AWS SSO Device Authorization flow must have an AWS SSO account with access to one or more AWS accounts.
+The user must also have a browser available to complete the device authorization flow. They will be prompted to visit a URL.
+
+That's it. No AWS CLI or other tools are required.
+
+## Usage
+
+There is a convenience function to avoid needing to instantiate an instance of the `AwsSsoDeviceAuthProvider` class if you only need to get credentials once and you already know the role you want to assume:
+
+```ts
+import { AwsSsoDeviceAuthProvider } from 'aws-sso-device-auth-provider'
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3"
+
+const s3Client = new S3Client({
+  region: 'eu-west-1',
+  credentials: await AwsSsoDeviceAuthProvider.getAwsCredentialIdentityProviderForRole({
+    startUrl: 'https://echobox.awsapps.com/start',
+    accountId: '060610571733',
+    roleName: 'AWSReadOnlyAccess'
+  })
+})
+
+console.log(await s3Client.send(new ListBucketsCommand({})))
+```
+
+Alternatively, you can instantiate the `AwsSsoDeviceAuthProvider` class and use it to get information about the user's AWS accounts and roles:
+
+```ts
+import { AwsSsoDeviceAuthProvider } from 'aws-sso-device-auth-provider'
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3"
+
+const provider = new AwsSsoDeviceAuthProvider({
+  startUrl: 'https://echobox.awsapps.com/start'
+})
+
+// Optional filters - with support for globs
+const accounts = await provider.getAccounts({accountName: 'staging-*'})
+const rolesForFirstAccount = await provider.getRolesForAccount({accountId: accounts[0].accountId, roleName: '*ReadOnly*'})
+
+const credentials = await provider.getAwsCredentialIdentityProviderForRole({
+  accountId: accounts[0].accountId,
+  roleName: rolesForFirstAccount[0].roleName
+})
+
+// It caches the credentials based on the expiry time in the response, so this following call would return the same credentials
+const s3Client = new S3Client({
+  region: 'eu-west-1',
+  credentials: provider.getAwsCredentialIdentityProviderForRole({
+    accountId: '060610571733',
+    roleName: 'AWSReadOnlyAccess'
+  })
+})
+```
---
+++ src/index.ts
@@ -0,0 +1,350 @@
+import { AccountInfo, GetRoleCredentialsCommand, paginateListAccountRoles, paginateListAccounts, RoleCredentials, SSOClient } from '@aws-sdk/client-sso'
+import { AuthorizationPendingException, CreateTokenCommand, CreateTokenResponse, ExpiredTokenException, InternalServerException, InvalidClientException, InvalidClientMetadataException, RegisterClientCommand, RegisterClientRequest, RegisterClientResponse, SlowDownException, SSOOIDCClient, SSOOIDCServiceException, StartDeviceAuthorizationCommand, UnauthorizedClientException } from '@aws-sdk/client-sso-oidc'
+import { AwsCredentialIdentityProvider } from '@smithy/types'
+import Keyv from 'keyv'
+import { KeyvFile } from 'keyv-file'
+import { minimatch } from 'minimatch'
+import { homedir } from 'os'
+import { join, dirname } from 'path'
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+const unixNow = () => Math.floor(Date.now() / 1000)
+const unixExpiresInMs = (seconds: number) => seconds * 1000
+const unixExpiresAtToExpiresInMs = (unixDate: number) => unixExpiresInMs(unixDate - unixNow())
+const msExpiresAtToExpiresInMs = (msDate: number) => msDate - Date.now()
+
+export interface AwsSsoDeviceAuthProviderOpts {
+  /**
+   * The URL to start the SSO login flow
+   * @example https://company.awsapps.com/start
+   */
+  startUrl: string
+  /**
+   * Options to pass to the {@link RegisterClientCommand}
+   */
+  registerClientOpts?: Partial<RegisterClientRequest>
+  /**
+   * Pass your own {@link SSOOIDCClient} instance for us to use.
+   */
+  ssoOidcClient?: SSOOIDCClient
+  /**
+   * Pass your own {@link SSOClient} instance for us to use.
+   */
+  ssoClient?: SSOClient
+  /**
+   * Pass a custom Keyv store, eg to use a different storage backend
+   */
+  keyv?: Keyv
+}
+
+export type getAwsCredentialIdentityProviderForRoleOpts = { roleName: string } & ({ accountId: number | string } | { accountName: string })
+
+export class AwsSsoDeviceAuthProvider implements AwsSsoDeviceAuthProviderOpts {
+
+  keyv: Keyv
+  registerClientOpts?: Partial<RegisterClientRequest>
+  startUrl: string
+  ssoOidcClient: SSOOIDCClient
+  ssoClient: SSOClient
+
+  /**
+   * Handles some common errors thrown by the SSO OIDC service
+   * @param e An error thrown by the SSO OIDC service
+   */
+  protected async ssoOidcGenericErrorHandler(e: SSOOIDCServiceException): Promise<void> {
+    switch (e.constructor) {
+      case AuthorizationPendingException:
+      case ExpiredTokenException:
+        await this.keyv.delete('auth_token')
+        this.newAccessToken()
+        break
+      case InternalServerException:
+      case SlowDownException:
+        await sleep(5000)
+        break
+      case InvalidClientException:
+      case InvalidClientMetadataException:
+      case UnauthorizedClientException:
+        await this.keyv.delete('client_registration')
+        this.getOrRegisterClient()
+        break
+    }
+    throw e
+  }
+
+  /**
+   * Returns the SSO OIDC client registration info, registering a new client if necessary
+   * @returns The client registration info
+   */
+  public async getOrRegisterClient(): Promise<RegisterClientResponse> {
+    let registrationInfo = await this.keyv.get<RegisterClientResponse>('client_registration')
+    if (!registrationInfo || registrationInfo.clientSecretExpiresAt! < unixNow()) {
+      const registerClientCommand = new RegisterClientCommand({
+        clientName: 'pulumi-aws-sso',
+        clientType: 'public',
+        ...this.registerClientOpts
+      })
+      registrationInfo = await this.ssoOidcClient.send(registerClientCommand)
+      await this.keyv.set('client_registration', registrationInfo, unixExpiresAtToExpiresInMs(registrationInfo.clientSecretExpiresAt!))
+    }
+    console.log('Registered client with ID:', registrationInfo.clientId)
+    return registrationInfo!
+  }
+
+  /**
+   * Attempts to refresh the access token using the provided refresh token.
+   * On failure, will fall back to requesting new credentials.
+   * @param refreshToken The refresh token to use to refresh the access token
+   * @returns The new access token
+   */
+  protected async refreshAccessToken(refreshToken: string): Promise<string> {
+    const registrationInfo = await this.getOrRegisterClient()
+    try {
+      const refreshTokenResponse = await this.ssoOidcClient.send(new CreateTokenCommand({
+        clientId: registrationInfo.clientId,
+        clientSecret: registrationInfo.clientSecret,
+        refreshToken: refreshToken,
+        grantType: 'refresh_token',
+        scope: ['aws:account:access']
+      }))
+      await this.keyv.set('auth_token', refreshTokenResponse, unixExpiresInMs(refreshTokenResponse.expiresIn!))
+      return refreshTokenResponse.accessToken!
+    } catch (e: unknown) {
+      if (!(e instanceof SSOOIDCServiceException)) throw e
+      return this.ssoOidcGenericErrorHandler(e)
+        .then(this.getAccessToken)
+    }
+  }
+
+  /**
+   * Requests a new access token using the device authorization flow
+   * @returns The new access token
+   */
+  protected async newAccessToken(): Promise<string> {
+    const registrationInfo = await this.getOrRegisterClient()
+    const startDeviceAuthorizationCommand = new StartDeviceAuthorizationCommand({
+      clientId: registrationInfo.clientId,
+      clientSecret: registrationInfo.clientSecret,
+      startUrl: this.startUrl
+    })
+    const deviceAuthorizationResponse = await this.ssoOidcClient.send(startDeviceAuthorizationCommand)
+    console.log('Please visit', deviceAuthorizationResponse.verificationUriComplete)
+    console.log('Now polling for authentication...\n')
+    while (true) {
+      await sleep(deviceAuthorizationResponse.interval! * 1000)
+      process.stdout.write('.')
+      try {
+        const tokenResponse = await this.ssoOidcClient.send(new CreateTokenCommand({
+          clientId: registrationInfo.clientId,
+          clientSecret: registrationInfo.clientSecret,
+          deviceCode: deviceAuthorizationResponse.deviceCode,
+          grantType: 'urn:ietf:params:oauth:grant-type:device_code',
+          scope: ['aws:account:access']
+        }))
+        await this.keyv.set('auth_token', tokenResponse, unixExpiresInMs(tokenResponse.expiresIn!))
+        return tokenResponse.accessToken!
+      } catch (e: unknown) {
+        if (e instanceof AuthorizationPendingException) {
+          continue
+        } else if (e instanceof SSOOIDCServiceException) {
+          return this.ssoOidcGenericErrorHandler(e)
+            .then(this.getAccessToken)
+        } else {
+          throw e
+        }
+      }
+    }
+  }
+
+  /**
+   * Returns an access token for use with the SSO service.
+   * @returns A saved access token, or a refreshed access token if we have a saved refresh token, or a new access token if we have no valid saved tokens.
+   */
+  public async getAccessToken(): Promise<string> {
+    let authTokenResponse = await this.keyv.get<CreateTokenResponse>('auth_token')
+    if (!authTokenResponse) {
+      return this.newAccessToken()
+    } else if (authTokenResponse.refreshToken) {
+      return this.refreshAccessToken(authTokenResponse.refreshToken)
+    }
+    return authTokenResponse!.accessToken!
+  }
+
+  /**
+   * Returns a list of accounts available to the authenticated user.
+   * @property opts
+   * @param opts.accountId Filter the returned list of accounts by account number
+   * @param opts.accountName Filter the returned list of accounts by account name (can be a {@link https://github.com/isaacs/minimatch|Minimatch}-compatible glob)
+   * @returns A list of account names and numbers matching the provided filters.
+   */
+  public async getAccounts({ accountId, accountName }: { accountId?: number | string, accountName?: string } = {}): Promise<AccountInfo[]> {
+    const accountListPaginator = paginateListAccounts({ client: this.ssoClient }, { accessToken: await this.getAccessToken() })
+    let accountList: AccountInfo[] = []
+
+    for await (const page of accountListPaginator) {
+      accountList.push(...page.accountList!)
+    }
+
+    if (accountList.length === 0) {
+      throw new Error(`No accounts found ${accountId || accountName ? `matching filters: ${accountId} ${accountName}` : ''}`)
+    }
+
+    if (accountId) {
+      accountList = accountList.filter((account) => account.accountId! === accountId.toString())
+    }
+    if (accountName) {
+      accountList = accountList.filter((account) => minimatch(account.accountName!, accountName))
+    }
+
+    return accountList
+  }
+
+  /**
+   * Returns the account ID for a single account with the given name. If multiple accounts are found, an error is thrown.
+   * @param accountName Filter the list of accounts by account name (can be a {@link https://github.com/isaacs/minimatch|Minimatch}-compatible glob)
+   * @returns The account ID of the account with the given name
+   */
+  public async getSingleAccountIdWithName(accountName: string): Promise<string> {
+    const accounts = await this.getAccounts({ accountName })
+    if (accounts.length > 1) {
+      throw new Error(`More than one account found with name ${accountName}: ${accounts.map(account => account.accountName).join(', ')}`)
+    }
+    return accounts[0].accountId!
+  }
+
+  /**
+   * Returns a list of roles available to the authenticated user for a given account ID.
+   * @property opts
+   * @param opts.accountId The ID of the account to list roles for
+   * @param opts.accountName The name of the account to list roles for (can be a {@link https://github.com/isaacs/minimatch|Minimatch}-compatible glob) (Note that this will require listing all accounts to find the correct one)
+   * @param opts.roleName Filter the returned list of roles by role name (can be a {@link https://github.com/isaacs/minimatch|Minimatch}-compatible glob)
+   * @returns An array of role names matching the provided filters, or all roles if no filters are provided.
+   */
+  public async getRolesForAccount({ accountName, roleName }: { accountName: string, roleName?: string }): Promise<string[]>
+  public async getRolesForAccount({ accountId, roleName }: { accountId: number | string, roleName?: string }): Promise<string[]>
+  public async getRolesForAccount({ accountId, accountName, roleName }: { accountId?: number | string, accountName?: string, roleName?: string }): Promise<string[]> {
+    if (!accountId && !accountName) {
+      throw new Error('Either accountId or accountName must be provided')
+    }
+    if (!accountId) {
+      accountId = await this.getSingleAccountIdWithName(accountName!)
+    }
+
+    const accountRolePaginator = paginateListAccountRoles({ client: this.ssoClient }, { accessToken: await this.getAccessToken(), accountId: accountId.toString() })
+    let accountRoles: string[] = []
+
+    for await (const page of accountRolePaginator) {
+      accountRoles.push(...page.roleList!.map((role) => role.roleName!))
+    }
+
+    if (accountRoles.length === 0) {
+      throw new Error(`No roles found for account ${accountId}`)
+    }
+
+    if (roleName) {
+      accountRoles = accountRoles.filter(minimatch.filter(roleName))
+    }
+
+    return accountRoles
+  }
+
+  /**
+   * Returns temporary credentials for a given role in a given account.
+   * @property opts
+   * @param opts.accountId The ID of the account to get credentials for
+   * @param opts.accountName The name of the account to get credentials for (can be a {@link https://github.com/isaacs/minimatch|Minimatch}-compatible glob)
+   * @param opts.roleName The name of the role to get credentials for (must be an exact match - use {@link getRolesForAccount} to find roles by pattern first)
+   */
+  public async getCredentialsForRole({ accountName, roleName }: { accountName: string, roleName: string }): Promise<Required<RoleCredentials>>
+  public async getCredentialsForRole({ accountId, roleName }: { accountId: number | string, roleName: string }): Promise<Required<RoleCredentials>>
+  public async getCredentialsForRole({ accountId, accountName, roleName }: { accountId?: number | string, accountName?: string, roleName: string }): Promise<Required<RoleCredentials>>
+  public async getCredentialsForRole({ accountId, accountName, roleName }: { accountId?: number | string, accountName?: string, roleName: string }): Promise<Required<RoleCredentials>> {
+    if (!accountId && !accountName) {
+      throw new Error('Either accountId or accountName must be provided')
+    }
+    if (!accountId) {
+      accountId = await this.getSingleAccountIdWithName(accountName!)
+    }
+
+    let credentials = await this.keyv.get<RoleCredentials>(`role_credentials_${accountId}_${roleName}`)
+
+    if (!credentials) {
+      const response = await this.ssoClient.send(new GetRoleCredentialsCommand({
+        accessToken: await this.getAccessToken(),
+        accountId: accountId.toString(),
+        roleName: roleName
+      }))
+
+      this.keyv.set(`role_credentials_${accountId}_${roleName}`, response.roleCredentials, msExpiresAtToExpiresInMs(response.roleCredentials!.expiration!))
+
+      credentials = response.roleCredentials
+    }
+
+    return credentials as Required<RoleCredentials>
+  }
+
+  /**
+   * Returns an {@link AwsCredentialIdentityProvider} that can be used to provide credentials for a given role in a given account.
+   * Can be used to provide credentials to the AWS SDK
+   * @example
+   * ```ts
+   *   import { S3Client } from '@aws-sdk/client-s3'
+   *   import { AwsSsoDeviceAuthProvider } from 'aws-sso-device-auth-provider'
+   *
+   *  const provider = new AwsSsoDeviceAuthProvider({
+   *   startUrl: 'https://echobox.awsapps.com/start'
+   *  })
+   *
+   *   const s3Client = new S3Client({
+   *     credentials: await provider.getAwsCredentialIdentityProviderForRole({
+   *       accountId: '123456789012',
+   *       roleName: 'MyRole'
+   *     })
+   *   })
+   * ```
+   * @property opts
+   * @param opts.accountId The ID of the account to get credentials for
+   * @param opts.accountName The name of the account to get credentials for (can be a {@link https://github.com/isaacs/minimatch|Minimatch}-compatible glob)
+   * @param opts.roleName The name of the role to get credentials for (must be an exact match - use {@link getRolesForAccount} to find roles by pattern first)
+   */
+  public async getAwsCredentialIdentityProviderForRole({ roleName, ...rest }: getAwsCredentialIdentityProviderForRoleOpts): Promise<AwsCredentialIdentityProvider> {
+    return async () => {
+      const credentials = await this.getCredentialsForRole({roleName, ...rest })
+      return {
+        ...credentials,
+        // It randomly wants an actual date here instead of a unix time like the rest of the SDK
+        expiration: new Date(credentials.expiration)
+      }
+    }
+  }
+
+  /**
+   * Convenience static method to create an {@link AwsCredentialIdentityProvider} for a given role in a given account using the given startURL in a single call.
+   * @see {@link getAwsCredentialIdentityProviderForRole}
+   * @see {@link AwsSsoDeviceAuthProvider}
+   */
+  public static async getAwsCredentialIdentityProviderForRole(opts: AwsSsoDeviceAuthProviderOpts & getAwsCredentialIdentityProviderForRoleOpts): Promise<AwsCredentialIdentityProvider> {
+    const provider = new AwsSsoDeviceAuthProvider(opts)
+    return provider.getAwsCredentialIdentityProviderForRole(opts)
+  }
+
+  constructor(opts: AwsSsoDeviceAuthProviderOpts) {
+    this.registerClientOpts = opts.registerClientOpts
+    this.startUrl = opts.startUrl
+
+    this.ssoOidcClient = opts.ssoOidcClient ?? new SSOOIDCClient({
+      region: process.env.AWS_REGION ?? process.env.AWS_DEFAULT_REGION ?? 'eu-west-1'
+    })
+
+    this.ssoClient = opts.ssoClient ?? new SSOClient({
+      region: process.env.AWS_REGION ?? process.env.AWS_DEFAULT_REGION ?? 'eu-west-1'
+    })
+
+    const defaultFileDir = (process.env.AWS_SHARED_CREDENTIALS_FILE || process.env.AWS_CONFIG_FILE) ? dirname(process.env.AWS_SHARED_CREDENTIALS_FILE! ?? process.env.AWS_CONFIG_FILE!) : join(homedir(), '.aws')
+    this.keyv = opts.keyv ?? new Keyv({
+      store: new KeyvFile({
+        filename: join(defaultFileDir, 'aws-sso-device-auth.json')
+      })
+    })
+  }
+}
